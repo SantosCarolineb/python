46e8942f527e9efbce20848bb8be0ab93e483ab4
Caroline Batista dos Santos <105455249+SantosCarolineb@users.noreply.github.com>
2022-05-13 22:17:41 -0300

    fiz o 14

--- exercicios/Exer-EstruturasCondicionais.py
+++ exercicios/Exer-EstruturasCondicionais.py
@@ -251,7 +251,35 @@ Condição: {condicao}
 
 # 14-Faça um Programa que peça os três lados de um triângulo. O programa deverá informar se os valores podem ser um triângulo. Indique, caso os lados formem um triângulo, se o mesmo é: equilátero, isósceles ou escaleno.
 
+print("Descubra qual é o triângulo informando a medida dos lados")
 
+lado1 = int(input("Lado 1: "))
+lado2 = int(input("Lado 2: "))
+lado3 = int(input("Lado 3: "))
+
+l1l2 = lado1+lado2
+
+if l1l2 > lado3:
+    if lado1 == lado2 and lado1 == lado3:
+        print("Triângulo Equilátero")
+    elif lado1 != lado2:
+        if lado3 == lado1 or lado3 == lado2:
+            print("Triângulo Isósceles")
+        else:
+            print("Triângulo Escaleno")
+    elif lado1 != lado3:
+        if lado2 == lado1 or lado2 == lado3:
+            print("Triângulo Isósceles")
+        else:
+            print("Triângulo Escaleno")
+    elif lado2 != lado3:
+        if lado1 == lado2 or lado1 == lado3:
+            print("Triângulo Isósceles")
+        else:
+            print("Triângulo Escaleno")
+
+else:
+    print("Não é um triângulo!")
 
 
 
